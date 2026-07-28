bca14dc6ce06c6fe2e5c65fe058d83cced956ba7
Shivam Ugale <ugaleshivamt50@gmail.com>
2026-07-28 22:28:34 +0530

    update basic .py

--- basic .py
+++ basic .py
@@ -4,3 +4,14 @@ if(a>b):
     print(" a is grater tahn b")
 else:
     print("b is greter than a")
+    #add and sub
+x=10
+y=5
+z=x+y   
+print("addition of x and y is:",z)
+z=x-y   
+print("subtraction of x and y is:",z)
+z=x*y
+print(" multiplication of x and y is:",z)    
+z=x/y
+print("division of x and y is:",z)
